b85645c0a5917684ab6ccad8e49c53cca207eebf
victormmcouto <myprogrammingthings@gmail.com>
2026-02-05 17:55:51 -0300

    Cria a estrutura da classe com seu atributo e dois métodos

---
+++ JavaCrudConsole/src/model/services/InventoryFileService.java
@@ -0,0 +1,21 @@
+package model.services;
+
+import java.io.IOException;
+
+import model.entities.Inventory;
+
+public class InventoryFileService {
+	private String filePath;
+	
+	public InventoryFileService(String filePath) {
+		this.filePath = filePath;
+	}
+	
+	public Inventory loadData() {
+
+	}
+	
+	public void saveData(Inventory inventory) {
+		
+	}
+}
