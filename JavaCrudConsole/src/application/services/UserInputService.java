91f98b9546e17a96b426bf5f17678c53d3cd19d5
victormmcouto <myprogrammingthings@gmail.com>
2026-02-06 15:57:28 -0300

    feat: Implementa método getProduct do serviço de UserInput

--- JavaCrudConsole/src/application/services/UserInputService.java
+++ JavaCrudConsole/src/application/services/UserInputService.java
@@ -3,10 +3,13 @@ package application.services;
 import java.util.Scanner;
 
 import application.enums.MenuOptions;
+import model.entities.Product;
+import model.enums.ProductCategory;
 
 public class UserInputService {
 	
-	public Scanner sc;
+	private Scanner sc;
+	private InputValidationService validator;
 	
 	public UserInputService(Scanner sc) {
 		this.sc = sc;
@@ -14,13 +17,26 @@ public class UserInputService {
 	
 	public MenuOptions getOption() {
 		while (true) {
-			System.out.print("Insira uma opção: ");
-			
+			System.out.print("Insira uma opção: ");			
 			try {
 				MenuOptions option = MenuOptions.valueOf(sc.next().toUpperCase());
 				return option;
 			} catch (IllegalArgumentException e) {
 				System.out.println("Insira uma opção válida!");
+			}
+		}
+	}
+	
+	public Product getProduct() {
+		while (true) {
+			try {
+				String name = validator.validateString(sc.next());
+				ProductCategory category = validator.validateEnum(sc.next(), ProductCategory.class);
+				Double price = validator.validateNumber(sc.next(), Double.class);
+				
+				return new Product(name, category, price);
+			} catch (IllegalArgumentException e) {
+				System.out.println("Valor inválido!!");
 			}
 		}
 	}
