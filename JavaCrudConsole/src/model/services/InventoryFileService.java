5d600d6655c11bfedbf29e1d8a475799b6485afa
Victor Couto <victormaiacouto00@gmail.com>
2026-02-07 21:11:47 -0300

    feat: Adicona tratamento de exceção para erro de parsing durante o load dos dados do inventário

--- JavaCrudConsole/src/model/services/InventoryFileService.java
+++ JavaCrudConsole/src/model/services/InventoryFileService.java
@@ -24,28 +24,33 @@ public class InventoryFileService {
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
 			String line = br.readLine();
+		
+			if (br.lines().count() == 0) {;
+				while(line != null) {
+					String[] record = line.split(",");
 					
-			while(line != null) {
-				String[] record = line.split(",");
-				
-				Integer id = Integer.parseInt(record[0]);
-				String name = record[1];
-				ProductCategory category = ProductCategory.valueOf(record[2]);
-				Double price = Double.valueOf(record[3]);
-				Integer quantity = Integer.valueOf(record[4]);
-				LocalDateTime timeModified = LocalDateTime.parse(record[5]);
-				
-				Product product = new Product(name, category, price);
-				ProductItem productItem = new ProductItem(id, product, quantity, timeModified);
-				
-				inventory.addNewProductItem(productItem);
-				
-				line = br.readLine();
+					Integer id = Integer.parseInt(record[0]);
+					String name = record[1];
+					ProductCategory category = ProductCategory.valueOf(record[2]);
+					Double price = Double.valueOf(record[3]);
+					Integer quantity = Integer.valueOf(record[4]);
+					LocalDateTime timeModified = LocalDateTime.parse(record[5]);
+					
+					Product product = new Product(name, category, price);
+					ProductItem productItem = new ProductItem(id, product, quantity, timeModified);
+					
+					inventory.addNewProductItem(productItem);
+					
+					line = br.readLine();
+				}
+			} else {
+				System.out.println("Inventório vazio!");
 			}
-		} catch (IOException e) {
+		} catch (IllegalArgumentException | IOException e) {
 			System.out.println("Não foi possível executar a leitura dos dados do arquivo " + filePath);
+			System.out.println("Arquivo mal formatado ou não existe!");
 			System.out.println(e.getMessage());
-		}
+		} 
 		
 		return inventory;
 	}
