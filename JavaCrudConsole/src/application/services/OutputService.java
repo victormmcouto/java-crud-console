191aa5d4a90bc2dcfaafebbc4cca71c5af2a249e
victormmcouto <myprogrammingthings@gmail.com>
2026-02-09 16:08:38 -0300

    feat: Acrescenta método printInputMessage à regra de negócio

--- JavaCrudConsole/src/application/services/OutputService.java
+++ JavaCrudConsole/src/application/services/OutputService.java
@@ -3,5 +3,6 @@ package application.services;
 public interface OutputService {
 	public void printMessage(String message);
 	public void printError(String message);
+	public void printInputMessage(String message);
 	public <E extends Enum<E>> void printEnum(Class<E> enumToShow, Boolean transpose);
 }
